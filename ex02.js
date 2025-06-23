071bbf54ae2416fb90aed3bd4c934a3f8bf5580f
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2025-06-23 17:24:06 -0300

    Update ex02.js

--- ex02.js
+++ ex02.js
@@ -11,3 +11,15 @@ let media = notas.reduce((acc, nota) => acc + nota, 0) / notas.length;
 //3 - Filtre as notas que são maiores ou iguais a 7 (notas de aprovação) usando o método filter() e armazene-as em um novo array chamado notasAprovadas. Exiba notasAprovadas.
 const notasAprovadas = notas.filter(nota => nota >= 7);
 console.log(`Notas aprovadas: ${notasAprovadas}`);
+
+//4 - Crie um novo array chamado situacaoAlunos usando o método map(). Para cada nota no array original notas:
+//Se a nota for maior ou igual a 7, o elemento correspondente em situacaoAlunos deve ser a string "Aprovado".
+//Caso contrário, o elemento deve ser a string "Reprovado". Exiba situacaoAlunos.
+
+const situacaoAlunos = notas.map(function (nota) {
+    if(nota >= 7){
+        return "Aprovado"
+    } else {
+        return "Reprovado"
+    }
+})
