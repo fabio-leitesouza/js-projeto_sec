a6b46140ff7faee0984849a3434757097ec866e2
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2025-06-26 21:04:08 -0300

    Update ex03.js

--- ex03.js
+++ ex03.js
@@ -24,3 +24,29 @@ const verificarPrimo = (numero) => {
     }
     return true;
 }
+
+// 4 - Peça ao usuário para digitar um número inteiro.
+
+let numeroUsuario = parseInt(prompt("Digite um número inteiro:"));
+
+// 5 - Utilize a função verificarPrimo() para determinar se o número digitado pelo usuário é primo. Exiba uma mensagem informando se o número é primo ou não.
+
+if (verificarPrimo(numeroUsuario)) {
+    console.log(`${numeroUsuario} é um número primo.`);
+} else {
+    console.log(`${numeroUsuario} não é um número primo.`);
+}
+
+// 6 - Crie um loop while que solicite ao usuário um número diferente de zero. O loop deve continuar pedindo um número até que o usuário digite 0. Para cada número diferente de 0 digitado, exiba se ele é "Positivo", "Negativo" ou "Zero".
+
+let numero = parseInt(prompt("Digite um número diferente de zero (0 para sair):"));
+
+while (numero !== 0) {
+    if (numero > 0) {
+        console.log(`${numero} é Positivo.`);
+    } else if (numero < 0) {
+        console.log(`${numero} é Negativo.`);
+    }
+    numero = parseInt(prompt("Digite um número diferente de zero (0 para sair):"));
+}
+
